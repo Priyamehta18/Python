9005e04aa2635664d4a00f8c2a4c0b0160173976
Priyamehta18 <kripriya.1913@gmail.com>
2024-11-27 10:30:34 +0530

    added new file

--- BiggestNumber.py
+++ BiggestNumber.py
@@ -1,7 +1,7 @@
-num1=int(input("Enter first number : "))
-num2=int(input("Enter second number : "))
-num3=int(input("Enter third number : "))
-num4=int(input("Enter fourth number : "))
+num1=int(input("Enter the first number : "))
+num2=int(input("Enter the second number : "))
+num3=int(input("Enter the third number : "))
+num4=int(input("Enter the fourth number : "))
 
 if num1>num2 and num1>num3 and num1>num4:
     big=num1
